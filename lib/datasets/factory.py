16c1475eb7231b8642b83471e8324035c62efb8a
Ning Ding <dingning.buaa@gmail.com>
2017-06-14 15:53:39 +0800

    Create factory.py

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -31,6 +31,10 @@ for year in ['2015']:
         name = 'coco_{}_{}'.format(year, split)
         __sets[name] = (lambda split=split, year=year: coco(split, year))
 
+for ImageSet in ['jiatu_9c']:
+    name = ImageSet
+    __sets[name] = (lambda name=name: own_data(name))
+        
 def get_imdb(name):
     """Get an imdb (image database) by name."""
     if not __sets.has_key(name):
